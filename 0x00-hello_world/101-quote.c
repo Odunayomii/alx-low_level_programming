9972e709ba63801767bf420974029bd8ee41031b
Odunayomii <salamiomobolanle12@gmail.com>
2022-03-10 13:48:53 -0800

    My new commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,10 @@
-#include <stdio.h>
 #include <unistd.h>
-
-/**
- * main - Entry point
- *
- * Description: 'and that piece of art is useful" - Dora Korpar, 2015-10-19'
- *
- * Return: Always 0 (Success)
- */
 
 int main(void)
 {
-	write(1, "and that piece of art is useful\" - Dora Korar, 2015-10-19\n",59);
-	return(1);
+		write(2,
+					      "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+					      	      59);
+
+			return (1);
+}
