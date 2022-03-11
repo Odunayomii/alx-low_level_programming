a64f4ff883a041c5dd63c0b6345173fbfb2b15ef
Odunayomii <salamiomobolanle12@gmail.com>
2022-03-11 14:46:27 -0800

    My commit 10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,4 +1,4 @@
-nclude <stdio.h>
+#include <stdio.h>
 
 /**
  * main - program that prints all possible different combinations of two digits
@@ -10,25 +10,25 @@ nclude <stdio.h>
 
 int main(void)
 {
-	int n1 = 48;
-	int n2;
-	int com = 44;
-	while (n1 <= 57)
-	{
-		n2 = n1 + 1;
-		while (n2 <= 57)
-		{
-			putchar(n1);
-			putchar(n2);
-			if (n1 != 56 || n2 != 57)
-			{
-				putchar(com);
-				putchar(32);
-			}
-			n2 += 1;
-		}
-		n1 += 1;
-	}
-	putchar('\n');
-	return (0);
+int n1 = 48;
+int n2;
+int com = 44;
+while (n1 <= 57)
+{
+n2 = n1 + 1;
+while (n2 <= 57)
+{
+putchar(n1);
+putchar(n2);
+if (n1 != 56 || n2 != 57)
+{
+putchar(com);
+putchar(32);
+}
+n2 += 1;
+}
+n1 += 1;
+}
+putchar('\n');
+return (0);
 }
