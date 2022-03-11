330ac4b326de564e8b284c8bb9292903fd9f3bbb
Odunayomii <salamiomobolanle12@gmail.com>
2022-03-11 09:32:01 -0800

    My commit 2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -2,10 +2,10 @@
 #include <ctype.h>
 
 /**
- * main - program that prints the alphabet in lowercase, followed by a new line
- * You can only use the putchar
- * Return: 0
- */
+* main - program that prints the alphabet in lowercase, followed by a new line
+* You can only use the putchar
+* Return: 0
+*/
 
 int main(void)
 {
