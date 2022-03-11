3ba957d7ba8c8961171269f3ed20c20d8d00e59e
Odunayomii <salamiomobolanle12@gmail.com>
2022-03-11 10:45:16 -0800

    My commit 2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -3,18 +3,18 @@
 
 /**
  * main - program that prints the alphabet in lowercase, followed by a new line
- * you can only use the putchar
+ * You can only use the putchar
  * Return: 0
  */
 
 int main(void)
 {
-int 1 = 'a';
-while (1 <= 'z')
-{
-putchar(1);
-1 += 1;
-}
-putchar('\n');
-return (0);
+	int l = 'a';
+	while (l <= 'z')
+	{
+		putchar(l);
+		l += 1;
+	}
+	putchar('\n');
+	return (0);
 }
