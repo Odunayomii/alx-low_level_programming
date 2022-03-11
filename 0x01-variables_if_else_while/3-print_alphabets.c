a526c755f4a7874feee1f96e83eef40840149403
Odunayomii <salamiomobolanle12@gmail.com>
2022-03-11 13:49:04 -0800

    My commit 3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,18 +8,18 @@
 
 int main(void)
 {
-	int lw = 'a';
-	int up = 'A';
-	while (lw <= 'z')
-	{
-		putchar(lw);
-		lw += 1;
-	}
-	while (up <= 'Z')
-	{
-		putchar(up);
-		up += 1;
-	}
-	putchar('\n');
-	return (0);
+int lw = 'a';
+int up = 'A';
+while (lw <= 'z')
+{
+putchar(lw);
+lw += 1;
+}
+while (up <= 'Z')
+{
+putchar(up);
+up += 1;
+}
+putchar('\n');
+return (0);
 }
