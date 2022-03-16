5a8aec63fc598e163b1c73d02f7e8489f7bf0fc1
odunayomii <salamiomobolanle12@gmail.com>
2022-03-16 14:09:22 +0100

    My main commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,20 @@
 #include "main.h"
 /**
- * main - Print "Holberton" followed by a new line.
+ * main - Print "_putchar" followed by a new line.
  * Description: You are not allowed to include standard libraries.
  * Return: 0
  */
 int main(void)
 {
-	char holberton[] = "Holberton";
+	char putchar[] = "_putchar";
 	int i = 0;
 
-	while (holberton[i] != '\0')
+	while (putchar[i] != '\0')
 	{
-		_putchar(holberton[i]);
+		_putchar(putchar[i]);
 		i++;
 	}
 	_putchar('\n');
 
 	return (0);
-}
+}}
