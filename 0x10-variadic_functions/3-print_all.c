1d4f34973b996a009170e28cec696f1a605c9bea
odunayomii <salamiomobolanle12@gmail.com>
2022-04-15 00:36:31 +0100

    A function that prints anything

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,61 @@
+#include "variadic_functions.h"
+#include<stdio.h>
+#include<stdarg.h>
+/**
+ * p_char - print char
+ * @list:arg
+ * Return: void
+ */
+
+void p_char(va_list list)
+{
+	printf("%c", va_arg(list, int));
+}
+/**
+ * p_string - print string
+ * @list:arg
+ * Return: void
+ */
+
+void p_string(va_list list)
+{
+	char *str;
+
+	str = va_arg(list, char*);
+	if (str == NULL)
+		str = "(nil)";
+printf("%s", str);
+}
+/**
+ * p_integer - print integer
+ * @list:arg
+ * Return: void
+ */
+
+void p_integer(va_list list)
+{
+	printf("%i", va_arg(list, int));
+}
+/**
+ * p_float - print float
+ * @list:arg
+ * Return: void
+ */
+
+void p_float(va_list list)
+{
+	printf("%f", va_arg(list, double));
+}
+/**
+ * print_all - print everything
+ * @format:arg
+ * Return: void
+ */
+void print_all(const char * const format, ...)
+
+{
+	unsigned int i, j;
+	t_print t[] = {
+		{"c", p_char},
+		{"s", p_string},
+
